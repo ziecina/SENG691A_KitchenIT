c5efdfed6392c337f545ff5e8711adcdeb5fa699
Mark Ziecina <ziecina@gmail.com>
2016-07-06 02:07:48 -0400

    Added a process UPC

---
+++ WebApp/process_UPC.php
@@ -0,0 +1,17 @@
+<?
+require 'aws/aws-autoloader.php';
+
+/*
+use Aws\DynamoDb\DynamoDbClient;
+
+$client = DynamoDbClient::factory(array(
+    'profile' => '<profile in your aws credentials file>',
+    'region'  => '<region name>'
+));*/
+
+
+echo $_REQUEST['UPC'];
+echo $_REQUEST['Action'];
+
+
+?>
