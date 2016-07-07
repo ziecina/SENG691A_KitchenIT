755ef49a3c13305e409ea9918fee0576a81a97fd
ziecina <ziecina@gmail.com>
2016-07-07 16:39:32 -0400

    changing mysqli to object

--- WebApp/process_UPC.php
+++ WebApp/process_UPC.php
@@ -2,7 +2,7 @@
 require 'aws/aws-autoloader.php';
 
 
-$link = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
+$link = new mysqli($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 
 $result = $link->query("SHOW TABLES;");
 
