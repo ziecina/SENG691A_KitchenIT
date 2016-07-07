b9f5a01cae6dcb082eefabf974237c185e3a9e44
ziecina <ziecina@gmail.com>
2016-07-07 16:36:48 -0400

    Testing mysqli_query

--- WebApp/process_UPC.php
+++ WebApp/process_UPC.php
@@ -4,6 +4,11 @@ require 'aws/aws-autoloader.php';
 
 $link = mysqli_connect($_SERVER['RDS_HOSTNAME'], $_SERVER['RDS_USERNAME'], $_SERVER['RDS_PASSWORD'], $_SERVER['RDS_DB_NAME'], $_SERVER['RDS_PORT']);
 
+$result = $link->query("SHOW TABLES;");
+
+$row = $results->fetch_object();
+print_r($row);
+
 
 echo $_REQUEST['UPC'];
 echo $_REQUEST['Action'];
